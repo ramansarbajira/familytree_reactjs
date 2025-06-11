3aee5ccd1a3644ebbd6b30a1012271a0ec442fe3
ramansarbajira <raman.s@sarbajira.com>
2025-06-11 19:41:17 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    historyApiFallback: true,
-  },
+  base: './',
 });
