b2980a793828a2cc590bc1f26f06deea15e3883b
ramansarbajira <raman.s@sarbajira.com>
2025-06-11 18:53:06 +0530

    changes updated

--- src/Components/AuthLogo.jsx
+++ src/Components/AuthLogo.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 const AuthLogo = () => {
   return (
     <div className="flex justify-center mb-1">
+      <a href='/'>
       <img
         src="/assets/logo.png"
         alt="Aalam Logo"
         className="w-40 h-40"
-      />
+      /></a>
     </div>
   );
 };
